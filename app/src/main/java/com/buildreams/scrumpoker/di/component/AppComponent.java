810b086b38dc75159d56a736075c67a784585806
Camilo Rodríguez Garzón <cam_rod@hotmail.com>
2019-10-05 21:25:29 -0300

    [created deployment card fragment and adapter] in order to show the card

--- app/src/main/java/com/buildreams/scrumpoker/di/component/AppComponent.java
+++ app/src/main/java/com/buildreams/scrumpoker/di/component/AppComponent.java
@@ -2,6 +2,7 @@ package com.buildreams.scrumpoker.di.component;
 
 import com.buildreams.scrumpoker.ScrumPokerApplication;
 import com.buildreams.scrumpoker.di.ActivityModule;
+import com.buildreams.scrumpoker.di.FragmentModule;
 import dagger.Component;
 import dagger.android.AndroidInjectionModule;
 import dagger.android.AndroidInjector;
@@ -13,6 +14,7 @@ import javax.inject.Singleton;
         modules = {
                 AndroidInjectionModule.class,
                 ActivityModule.class,
+                FragmentModule.class
         })
 public interface AppComponent extends AndroidInjector<ScrumPokerApplication> {
 
--- app/src/main/java/com/buildreams/scrumpoker/view/DeploymentCardActivity.kt
+++ app/src/main/java/com/buildreams/scrumpoker/view/DeploymentCardActivity.kt
@@ -1,17 +1,14 @@
 package com.buildreams.scrumpoker.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
 import com.buildreams.scrumpoker.R
 import com.buildreams.scrumpoker.databinding.ActivityDeploymentCardBinding
-import com.buildreams.scrumpoker.domain.entity.Card
-import com.buildreams.scrumpoker.view.adapter.DeploymentCardAdapter
+import com.buildreams.scrumpoker.view.fragment.DeploymentCardFragment
 import com.buildreams.scrumpoker.viewModel.UserViewModel
 import dagger.android.AndroidInjection
-import java.util.ArrayList
 import javax.inject.Inject
 
 class DeploymentCardActivity : AppCompatActivity() {
@@ -21,9 +18,6 @@ class DeploymentCardActivity : AppCompatActivity() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
-    @Inject
-    lateinit var adapter: DeploymentCardAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
@@ -31,22 +25,12 @@ class DeploymentCardActivity : AppCompatActivity() {
         val model: UserViewModel = ViewModelProvider(this, viewModelFactory).get(UserViewModel::class.java)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_deployment_card)
-        val card = Card(1, "1", 0)
-        binding.card = card
-        binding.name = model.user
 
-        val recyclerView = binding.rvPresentCard
+        addFragment(DeploymentCardFragment(this))
+    }
 
-        recyclerView.layoutManager = GridLayoutManager(this, 2)
-        val cards = ArrayList<Card>()
-        val img: Byte = 0
-        val card1 = Card(1, "test1", img)
-        val card2 = Card(2, "test2", img)
-        val card3 = Card(3, "test3", img)
-        cards.add(card1)
-        cards.add(card2)
-        cards.add(card3)
-        adapter.setCards(cards)
-        recyclerView.adapter = adapter
+    private fun addFragment(fragment: DeploymentCardFragment) {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fl_deployment_card, fragment).commit()
     }
 }
